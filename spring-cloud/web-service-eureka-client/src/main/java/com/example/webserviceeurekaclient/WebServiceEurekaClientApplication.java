f595ff15eb008d0895fc2669d418c78d6d5fc2e4
sochinlar <408448680@qq.com>
2018-07-01 14:22:12 +0800

    注册中心配置

---
+++ spring-cloud/web-service-eureka-client/src/main/java/com/example/webserviceeurekaclient/WebServiceEurekaClientApplication.java
@@ -0,0 +1,12 @@
+package com.example.webserviceeurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceEurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceEurekaClientApplication.class, args);
+    }
+}
